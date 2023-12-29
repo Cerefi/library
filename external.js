3802abbf2a79ba88ae8fbe47337192d1fc4ceff7
Fercie <ferciecaseria.xix@gmail.com>
2023-12-29 15:09:41 +0800

    Create function addBook
    Crate addBook function that executes when the dialog trigger close event

--- external.js
+++ external.js
@@ -3,13 +3,11 @@ const addButton = document.querySelector('#addButton');
 const bookTitle = document.querySelector('#bookTitle');
 const bookAuthor = document.querySelector('#bookAuthor');
 const bookPages = document.querySelector('#bookPages');
+const books = document.querySelector('.books');
+const card = document.querySelector(".card");
 const dialog = document.querySelector('#dialog');
 
 const myLibrary = [];
-
-addButton.addEventListener("click", ()=>{
-    dialog.showModal();
-});
 
 function Book(name, author, pages) {
     this.title = name;
@@ -18,11 +16,36 @@ function Book(name, author, pages) {
     this.read = "Not Read";
 }
 
+function addBooks(item){
+    const bookCard = document.createElement('div');
+        const title = document.createElement('p');
+        const author = document.createElement('p');
+        const pages = document.createElement('p');
+
+        bookCard.classList.add('card');
+
+        title.textContent = `Title: ${item.title}`;
+        author.textContent = `Author: ${item.author}`;
+        pages.textContent = `Pages: ${item.pages}`;
+        
+        bookCard.appendChild(title);
+        bookCard.appendChild(author);
+        bookCard.appendChild(pages);
+        books.appendChild(bookCard);
+
+}
+
+addButton.addEventListener("click", ()=>{
+    dialog.showModal();
+});
+
 add.addEventListener("click", ()=>{
     const book = new Book(bookTitle.value, bookAuthor.value, bookPages.value);
     myLibrary.push(book);
-})
+});
 
 dialog.addEventListener("close", ()=>{
-    console.log(myLibrary[0]);
-})
+    for (item of myLibrary){
+        addBooks(item);
+    }
+});
